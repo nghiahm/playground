95e59d27c82b57ec2a407f30de29e840592ba443
nghiahm <honghia06@gmail.com>
2026-09-10 16:34:42 +0000

    wip

--- angular-ui-bootstrap-react/app/components/date-picker/date-picker-day-view.tsx
+++ angular-ui-bootstrap-react/app/components/date-picker/date-picker-day-view.tsx
@@ -5,16 +5,32 @@ import {
   useDayPicker,
   type DayButtonProps,
   type MonthCaptionProps,
+  type MonthProps,
+  type WeekNumberProps,
+  type WeekdayProps,
+  type WeekdaysProps,
 } from "@daypicker/react";
 
 import { cn } from "~/lib/utils";
 
-function CustomDayButton({ modifiers, day, ...props }: DayButtonProps) {
+function CustomDayButton({
+  modifiers,
+  day,
+  onClick,
+  ...props
+}: DayButtonProps) {
   return (
     <DayButton
       {...props}
       day={day}
       modifiers={modifiers}
+      onClick={(event) => {
+        const monthGrid =
+          event.currentTarget.closest<HTMLElement>(".uib-daypicker");
+
+        onClick?.(event);
+        window.requestAnimationFrame(() => monthGrid?.focus());
+      }}
       className={cn(
         props.className,
         "btn btn-default btn-sm",
@@ -33,45 +49,112 @@ function CustomDayButton({ modifiers, day, ...props }: DayButtonProps) {
   );
 }
 
-function CustomMonthCaption({ calendarMonth, ...props }: MonthCaptionProps) {
-  const { goToMonth, nextMonth, previousMonth } = useDayPicker();
+function HiddenMonthCaption(_: MonthCaptionProps) {
+  return <></>;
+}
+
+function CustomWeekdays(props: WeekdaysProps) {
+  return <tr {...props} />;
+}
+
+function CustomMonth({ calendarMonth, children, ...props }: MonthProps) {
+  const { components, dayPickerProps, goToMonth, nextMonth, previousMonth } =
+    useDayPicker();
+  const monthGrid = React.Children.toArray(children).find(
+    (child) =>
+      React.isValidElement(child) && child.type === components.MonthGrid,
+  ) as
+    | React.ReactElement<React.TableHTMLAttributes<HTMLTableElement>>
+    | undefined;
+
+  if (!monthGrid) {
+    return <div {...props} />;
+  }
+
+  const gridChildren = React.Children.toArray(monthGrid.props.children);
+  const weekdays = gridChildren.find(
+    (child) =>
+      React.isValidElement(child) && child.type === components.Weekdays,
+  );
+  const weeks = gridChildren.find(
+    (child) => React.isValidElement(child) && child.type === components.Weeks,
+  );
 
   return (
-    <div
-      {...props}
-      className={cn("tw:grid tw:grid-cols-8 tw:items-center", props.className)}
-    >
-      <button
-        type="button"
-        className="btn btn-default btn-sm uib-left"
-        onClick={() => previousMonth && goToMonth(previousMonth)}
-        disabled={!previousMonth}
-        aria-label="Previous month"
-      >
-        <i className="glyphicon glyphicon-chevron-left" aria-hidden="true" />
-      </button>
-      <button
-        type="button"
-        className="btn btn-default btn-sm uib-title tw:col-span-6"
-        aria-live="assertive"
-      >
-        <strong>
-          {calendarMonth.date.toLocaleDateString("en-US", {
-            month: "long",
-            year: "numeric",
-          })}
-        </strong>
-      </button>
-      <button
-        type="button"
-        className="btn btn-default btn-sm uib-right"
-        onClick={() => nextMonth && goToMonth(nextMonth)}
-        disabled={!nextMonth}
-        aria-label="Next month"
-      >
-        <i className="glyphicon glyphicon-chevron-right" aria-hidden="true" />
-      </button>
+    <div {...props}>
+      {React.cloneElement(
+        monthGrid,
+        { tabIndex: -1 },
+        <thead>
+          <tr>
+            <th>
+              <button
+                type="button"
+                className="btn btn-default btn-sm pull-left uib-left"
+                onClick={() => previousMonth && goToMonth(previousMonth)}
+                disabled={!previousMonth}
+                aria-label="Previous month"
+              >
+                <i
+                  className="glyphicon glyphicon-chevron-left"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">previous</span>
+              </button>
+            </th>
+            <th colSpan={dayPickerProps.showWeekNumber ? 6 : 5}>
+              <button
+                type="button"
+                className="btn btn-default btn-sm uib-title"
+                role="heading"
+                aria-live="assertive"
+                aria-atomic="true"
+              >
+                <strong>
+                  {calendarMonth.date.toLocaleDateString("en-US", {
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </strong>
+              </button>
+            </th>
+            <th>
+              <button
+                type="button"
+                className="btn btn-default btn-sm pull-right uib-right"
+                onClick={() => nextMonth && goToMonth(nextMonth)}
+                disabled={!nextMonth}
+                aria-label="Next month"
+              >
+                <i
+                  className="glyphicon glyphicon-chevron-right"
+                  aria-hidden="true"
+                />
+                <span className="sr-only">next</span>
+              </button>
+            </th>
+          </tr>
+          {weekdays}
+        </thead>,
+        weeks,
+      )}
     </div>
+  );
+}
+
+function CustomWeekNumber({ children, ...props }: WeekNumberProps) {
+  return (
+    <th {...props} className={cn("text-center h6", props.className)}>
+      <em>{children}</em>
+    </th>
+  );
+}
+
+function CustomWeekday({ children, ...props }: WeekdayProps) {
+  return (
+    <th {...props} className={cn("text-center", props.className)}>
+      <small>{children}</small>
+    </th>
   );
 }
 
@@ -97,13 +180,19 @@ export function DatePickerDayView() {
       }}
       classNames={{
         root: "well well-sm uib-datepicker tw:inline-block",
+        month: "",
+        months: "",
         day: "uib-day text-center",
         weekday: "text-center",
         month_grid: "uib-daypicker",
       }}
       components={{
         DayButton: CustomDayButton,
-        MonthCaption: CustomMonthCaption,
+        Month: CustomMonth,
+        MonthCaption: HiddenMonthCaption,
+        Weekday: CustomWeekday,
+        WeekNumber: CustomWeekNumber,
+        Weekdays: CustomWeekdays,
       }}
       formatters={{
         formatWeekdayName: (date) =>
